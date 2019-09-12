046fbebaa2c61790e2cc13e7a81fda413cde78a2
dawei126156 <dawei126156@163.com>
2019-09-12 08:00:26 +0800

    整理

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,5 +1,5 @@
 import {observable, action} from 'mobx'
-import {login,getuserdata,getid,getapi,getport,getview,getpower} from '../../service/index'
+import {login,getuserdata,getid,getapi,getport,getview,getpower,getUserInfo,getViewAuthority} from '../../service/index'
 import {setToken, removeToken} from '../../utils/index'
 import {HttpInfo, HttpType, LoginForm} from '../../types/index'
 let account = {};
@@ -9,6 +9,8 @@ if (window.localStorage.getItem('account')){
 class User{
     @observable isLogin: boolean = false;
     @observable account: any = account;
+    @observable userInfo: any = {};
+    @observable viewAuthority: object[] = []; 
     @action async login(form: any): Promise<any>{
         let result: any = await login(form);
                if (result.code === 1){
@@ -51,6 +53,19 @@ class User{
     }
     @action async logout():Promise<any>{
         removeToken();
+    }
+    @action async getUserInfo(): Promise<any>{
+        let userInfo:any = await getUserInfo();
+        console.log('userInfo...', userInfo);
+        this.userInfo = userInfo.data;
+        this.getViewAuthority();
+    }
+
+    // 获取用户权限
+    @action async getViewAuthority(): Promise<any>{
+        let viewAuthority: any = await getViewAuthority();
+        console.log('viewAuthority...', viewAuthority);
+        this.viewAuthority = viewAuthority.data;
     }
 }
 
