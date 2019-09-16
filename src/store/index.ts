0767d7daf3573272e406428bc64439e2f7493a8e
luxuan <1131576937@qq.com>
2019-09-16 17:10:13 +0800

    luxuan

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,13 +2,16 @@ import User from './modules/user';
 import Question from './modules/question';
 import Manage from "./modules/manage"
 import Exam from "./modules/exam"
+import Stayclass from './modules/stayclass'
 const user = new User();
 const question = new Question();
 const manage=new Manage()
 const exam=new Exam()
+const stayclass = new Stayclass()
 export default {
     user,
     question,
     manage,
-    exam
+    exam,
+    stayclass
 }
